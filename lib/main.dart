fc5dd2b7f714bfe0446a14259242f7b87266ae4f
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2025-12-16 22:46:05 +0800

    Started with search bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,100 +4,93 @@ void main() {
   runApp(const MyApp());
 }
 
+String? title = 'Handbook';
+String? schoolLogo = 'assets/images/usls.png';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can  be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(
-          seedColor: Colors.green,
-          brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.white,
+          brightness: Brightness.light,
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Image.asset(
-        'assets/images/bg.jpg',
-        height: double.infinity,
-        fit: BoxFit.cover,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            title!,
+            style: TextStyle(
+              fontSize: 24,
+              fontFamily: 'Gilgan',
+              fontWeight: FontWeight.w400,
+              color: Colors.white,
+            ),
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Image.asset(
+                'assets/images/usls.png',
+                height: 75,
+                width: 75,
+              ),
+            ),
+          ],
+          iconTheme: IconThemeData(color: Colors.white),
+          centerTitle: false,
+          backgroundColor: const Color(0xFF077428),
+        ),
+        drawer: SafeArea(child: Drawer(surfaceTintColor: Colors.white)),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 22.0, horizontal: 40),
+          child: Stack(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: 58,
+                    width: 280,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'What do you want to know?',
+                        hintStyle: TextStyle(
+                          fontFamily: 'IBM Plex Sans',
+                          fontSize: 16,
+                          color: Color(0xFF02542D),
+                        ),
+                        suffixIcon: Icon(
+                          Icons.search,
+                          color: Color(0xFF02542D),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: Color(0xFF077428),
+                          ),
+                        ),
+                        filled: false,
+                        fillColor: Colors.grey,
+                      ),
+                      onChanged: (value) => (),
+                    ),
+                  ),
+                  IconButton(
+                    padding: EdgeInsets.only(left: 16),
+                    icon: Icon(Icons.info, color: Colors.grey, size: 32),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
